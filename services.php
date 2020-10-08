48aa7c7a46719b1960a6d5bf3b616775d0760b93
Herte <herte@rospace.ro>
2020-10-08 21:16:08 +0300

    metadata description

---
+++ services.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once "assets/php/functions.php";
+
+    $services=file_get_contents('assets/html/services.html');
+    $topBar=file_get_contents('assets/html/topBar.html');
+    $header=file_get_contents('assets/html/header.html');
+    $serviceSection=file_get_contents('assets/html/serviceSection.html');
+    $footer=file_get_contents('assets/html/footer.html');
+
+    $services=str_replace('{{ topBar }}', $topBar, $services);
+    $services=str_replace('{{ header }}', $header, $services);
+    $services=str_replace('{{ service }}', $serviceSection, $services);
+    $services=str_replace('{{ footer }}', $footer, $services);
+
+    echo $services;
+
+
+
+?>

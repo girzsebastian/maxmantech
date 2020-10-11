ef6f83daee27f75b2b1241a5d3ff6ffb186e518f
sebi <sebi@rospace.com>
2020-10-11 21:29:43 +0300

    Service

--- services.php
+++ services.php
@@ -4,6 +4,7 @@
 
     $services=file_get_contents('assets/html/services.html');
     $topBar=file_get_contents('assets/html/topBar.html');
+    $about=file_get_contents('assets/html/aboutSection.html');
     $header=file_get_contents('assets/html/header.html');
     $serviceSection=file_get_contents('assets/html/serviceSection.html');
     $footer=file_get_contents('assets/html/footer.html');
@@ -11,6 +12,7 @@
     $services=str_replace('{{ topBar }}', $topBar, $services);
     $services=str_replace('{{ header }}', $header, $services);
     $services=str_replace('{{ service }}', $serviceSection, $services);
+    $services=str_replace('{{ about }}', $about, $services);
     $services=str_replace('{{ footer }}', $footer, $services);
 
     echo $services;

ac251b0e1bb69fc51a3149a1821972c16cc7f5e3
sebi <sebi@rospace.com>
2020-10-26 15:56:17 +0200

    Add

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -19,10 +19,15 @@ function buildCategoryByName(array $categoryArray)
         foreach ($categoryArray as $innerArray) {
             if (!empty($innerArray)) {
                 foreach ($innerArray as $key => $value) {
-                    if (array_key_exists("image", $value) &&
+                   if (array_key_exists("image", $value) &&
                         array_key_exists("title", $value) &&
                         array_key_exists("cod", $value) &&
-                        array_key_exists("greutate", $value) &&
+//                        array_key_exists("greutate", $value) &&
+//                        array_key_exists("capacitate", $value) &&
+//                       array_key_exists("cursa", $value) &&
+//                       array_key_exists("viteza", $value) &&
+//                       array_key_exists("lungime", $value) &&
+//                       array_key_exists("tensiune", $value) &&
                         array_key_exists("id", $value)) {
                         if ($value['category'] == 'Banda transportatoare' or $value['category'] == 'Malaxor beton' or $value['category'] == 'Presa pavaje' or $value['category'] == 'Statie de beton') {
                             $file = file_get_contents('assets/html/productCard2.html');
@@ -30,7 +35,15 @@ function buildCategoryByName(array $categoryArray)
                                 ['{{ image }}', '{{ title }}', '{{ cod }}', '{{ inaltime }}', '{{ lungime }}', '{{ latime }}', '{{ greutate }}', '{{ id }}', '{{ price }}'],
                                 [$value['image'], $value['title'], $value['cod'], $value['inaltime'], $value['lungime'], $value['latime'], $value['greutate'], $value['id'], $value['price']], $file);
                             $category[$key] = $file;
-                        } else {
+                        }
+                        if ($value['category'] == 'Electrostivuitoare'){
+                            $file = file_get_contents('assets/html/productCard4.html');
+                            $file = str_replace(
+                                ['{{ image }}', '{{ title }}', '{{ cod }}', '{{ capacitate }}', '{{ cursa }}', '{{ viteza }}', '{{ lungime }}', '{{ tensiune }}','{{ id }}', '{{ price }}'],
+                                [$value['image'], $value['title'], $value['cod'], $value['capacitate'], $value['cursa'], $value['viteza'], $value['lungime'], $value['tensiune'],$value['id'], $value['price']], $file);
+                            $category[$key] = $file;
+                        }
+                        else {
                             $file = file_get_contents('assets/html/productCard.html');
                             $file = str_replace(
                                 ['{{ image }}', '{{ title }}', '{{ cod }}', '{{ putere }}', '{{ greutate }}', '{{ id }}'],
@@ -133,6 +146,26 @@ function getProductById(string $product, array $array)
                                         $resultSet['meta'] = $value2['meta'];
                                         return $resultSet;
                                     }
+                                }
+                                if ($value2['category'] == 'Electrostivuitoare'){
+                                    if ($product == $value2['id']) {
+                                        $resultSet['id'] = $value2['id'];
+                                        $resultSet['category'] = $value2['category'];
+                                        $resultSet['title'] = $value2['title'];
+                                        $resultSet['cod'] = $value2['cod'];
+                                        $resultSet['viteza'] = $value2['viteza'];
+                                        $resultSet['capacitate'] = $value2['capacitate'];
+                                        $resultSet['lungime'] = $value2['lungime'];
+                                        $resultSet['cursa'] = $value2['cursa'];
+                                        $resultSet['tensiune'] = $value2['tensiune'];
+                                        $resultSet['price'] = $value2['price'];
+                                        $resultSet['image'] = $value2['image'];
+                                        $resultSet['images'] = $value2['images'];
+                                        $resultSet['descriere'] = $value2['descriere'];
+                                        $resultSet['descriereText'] = $value2['descriereText'];
+                                        $resultSet['meta'] = $value2['meta'];
+                                        return $resultSet;
+                                    }
                                 } else {
                                     if ($product == $value2['id']) {
                                         $resultSet['id'] = $value2['id'];
@@ -227,7 +260,18 @@ function buildProductByName(array $productArray, array $descriptionArray)
             [$productArray['image'], $productArray['title'], $productArray['cod'], $productArray['greutate'], $productArray['inaltime'], $productArray['latime'], $productArray['lungime'], $productArray['descriere'], $descriere2, $images], $file);
         $product[] = $file;
         return implode('', $product);
-    } else {
+    }
+    if ($productArray['category'] == 'Electrostivuitoare') {
+        $product = [];
+        $file = file_get_contents('assets/html/productBody4.html');
+        $descriere2 = descriere2($descriptionArray);
+        $images = images($productArray);
+        $file = str_replace(
+            ['{{ image }}', '{{ title }}', '{{ cod }}', '{{ capacitate }}', '{{ cursa }}', '{{ viteza }}', '{{ lungime }}', '{{ tensiune }}','{{ descriere }}', '{{ descriereText }}', '{{ images }}', '{{ price }}'],
+            [$productArray['image'], $productArray['title'], $productArray['cod'], $productArray['capacitate'], $productArray['cursa'], $productArray['viteza'], $productArray['lungime'], $productArray['tensiune'], $productArray['descriere'], $descriere2, $images, $productArray['price']], $file);
+        $product[] = $file;
+        return implode('', $product);
+    }else {
         $product = [];
         $file = file_get_contents('assets/html/productBody.html');
         $descriere = descriere($descriptionArray);
